ea12efd6c0f0ba3826607c5274b412212ac1cc92
Max Lutsenko <max.lutsenko@akelius.com>
2026-03-01 01:35:31 +0200

    feat(contact): improve contact section layout and functionality

--- src/app/home/home-contact/home-contact.component.ts
+++ src/app/home/home-contact/home-contact.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { ALEX_DETAILS } from '../../shared/constants';
+import { LinkService } from '../../core/services';
 
 @Component({
   selector: 'app-home-contact',
-  imports: [],
+  imports: [MatButton, MatIcon],
   templateUrl: './home-contact.component.html',
   styleUrl: './home-contact.component.scss',
 })
-export class HomeContactComponent {}
+export class HomeContactComponent {
+  protected readonly links = ALEX_DETAILS.links;
+
+  private readonly linkService = inject(LinkService);
+
+  protected onLinkedInClick(): void {
+    this.linkService.getLinkedInLink();
+  }
+
+  protected onEmailClick(): void {
+    this.linkService.getEmailLink();
+  }
+}
