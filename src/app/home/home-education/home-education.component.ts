887d6750bdbed94c81594ba347b91503448a6ef5
Max Lutsenko <max.lutsenko@akelius.com>
2026-02-28 19:13:47 +0200

    feat(home): update education section with new layout and data

--- src/app/home/home-education/home-education.component.ts
+++ src/app/home/home-education/home-education.component.ts
@@ -1,4 +1,28 @@
 import { Component } from '@angular/core';
+import { it } from 'vitest';
+
+interface EducationItem {
+  position: string;
+  duration: string;
+  grade?: string;
+}
+
+const EDUCATION_ITEMS: EducationItem[] = [
+  {
+    position: 'Google UX Design Certification',
+    duration: 'August 2024 - March 2026',
+  },
+  {
+    position: 'NUUPES',
+    duration: 'Sep 2019 - March 2021',
+    grade: 'Master Degree in Sport Sciences',
+  },
+  {
+    position: 'NUUPES',
+    duration: 'August 2015 - March 2019',
+    grade: 'Bachelor Degree in Sport Sciences',
+  },
+];
 
 @Component({
   selector: 'app-home-education',
@@ -7,5 +31,6 @@ import { Component } from '@angular/core';
   styleUrl: './home-education.component.scss',
 })
 export class HomeEducationComponent {
-  protected educationItems = [];
+  protected educationItems = EDUCATION_ITEMS;
+  protected readonly it = it;
 }
